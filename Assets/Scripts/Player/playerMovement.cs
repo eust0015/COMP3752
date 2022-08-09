2fc96ef02724568d4cb4381009c36a36fbd9e6a2
Alex M <43807058+Evi1Cat@users.noreply.github.com>
2022-08-09 13:01:45 +0930

    Completed Basic Movement

--- Assets/Scripts/Player/playerMovement.cs
+++ Assets/Scripts/Player/playerMovement.cs
@@ -2,23 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class playerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private string leftKey = "a";
+    private string rightKey = "d";
+    private string upKey = "w";
+    private string downKey = "s";
     public Rigidbody2D player;
     public float moveSpeed = 5;
+    private float directionY = 0;
+    private float directionX = 0;
     void Start()
     {
 
     }
 
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("W"))
+        MovementDirection();
+        player.velocity = new Vector2(directionY * moveSpeed, player.velocity.y);
+        player.velocity = new Vector2(directionX * moveSpeed, player.velocity.x);
+    }
+
+    void MovementDirection()
+    {
+        if (Input.GetKeyDown(upKey))
         {
-            player.velocity = new Vector2(moveSpeed, player.velocity.y);
+            directionY = 1;
+        }
+        if (Input.GetKeyDown(downKey))
+        {
+            directionY = -1;
+        }
+        if (Input.GetKeyDown(leftKey))
+        {
+            directionX = -1;
+        }
+        if (Input.GetKeyDown(rightKey))
+        {
+            directionX = 1;
+        }
+        if (Input.GetKeyUp(upKey) || Input.GetKeyUp(downKey))
+        {
+            directionY = 0;
+        }
+        if (Input.GetKeyUp(leftKey) || Input.GetKeyUp(rightKey))
+        {
+            directionX = 0;
         }
     }
 }
