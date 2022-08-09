2b18d7a26f224e79dffbf20acb83ef3e7fde0844
Alex M <43807058+Evi1Cat@users.noreply.github.com>
2022-08-09 14:38:44 +0930

    Finished the basic movement

--- Assets/Scripts/Player/playerMovement.cs
+++ Assets/Scripts/Player/playerMovement.cs
@@ -9,7 +9,10 @@ public class playerMovement : MonoBehaviour
     private string upKey = "w";
     private string downKey = "s";
     public Rigidbody2D player;
-    public float moveSpeed = 5;
+    public float maxSpeed = 5;
+    public float acceleration = 5;
+    private float xspeed = 0;
+    private float yspeed = 0;
     private float directionY = 0;
     private float directionX = 0;
     void Start()
@@ -20,35 +23,92 @@ public class playerMovement : MonoBehaviour
     void Update()
     {
         MovementDirection();
-        player.velocity = new Vector2(directionY * moveSpeed, player.velocity.y);
-        player.velocity = new Vector2(directionX * moveSpeed, player.velocity.x);
+        player.velocity = new Vector2(directionY * yspeed, player.velocity.y);
+        player.velocity = new Vector2(directionX * xspeed, player.velocity.x);
     }
 
     void MovementDirection()
     {
-        if (Input.GetKeyDown(upKey))
+        if (Input.GetKey(upKey) | Input.GetKey(downKey))
         {
             directionY = 1;
+            if (Input.GetKey(upKey) & Input.GetKey(downKey))
+            {
+                if (yspeed > 0)
+                {
+                    yspeed -= acceleration * Time.deltaTime;
+                }
+                if (yspeed < 0)
+                {
+                    yspeed += acceleration * Time.deltaTime;
+                }
+            }
+            else if (Input.GetKey(upKey))
+            {
+                if (yspeed < maxSpeed)
+                {
+                    yspeed += acceleration * Time.deltaTime;
+                }
+            }
+            else if (Input.GetKey(downKey))
+            {
+                if (yspeed > -maxSpeed)
+                {
+                    yspeed -= acceleration * Time.deltaTime;
+                }
+            }
         }
-        if (Input.GetKeyDown(downKey))
+        else
         {
-            directionY = -1;
+            if (yspeed > 0)
+            {
+                yspeed -= acceleration * Time.deltaTime;
+            }
+            if (yspeed < 0)
+            {
+                yspeed += acceleration * Time.deltaTime;
+            }
         }
-        if (Input.GetKeyDown(leftKey))
-        {
-            directionX = -1;
-        }
-        if (Input.GetKeyDown(rightKey))
+
+        if (Input.GetKey(leftKey) | Input.GetKey(rightKey))
         {
             directionX = 1;
+            if (Input.GetKey(leftKey) & Input.GetKey(rightKey))
+            {
+                if (xspeed > 0)
+                {
+                    xspeed -= acceleration * Time.deltaTime;
+                }
+                if (xspeed < 0)
+                {
+                    xspeed += acceleration * Time.deltaTime;
+                }
+            }
+            else if (Input.GetKey(leftKey))
+            {
+                if (xspeed > -maxSpeed)
+                {
+                    xspeed -= acceleration * Time.deltaTime;
+                }
+            }
+            else if (Input.GetKey(rightKey))
+            {
+                if (xspeed < maxSpeed)
+                {
+                    xspeed += acceleration * Time.deltaTime;
+                }
+            }
         }
-        if (Input.GetKeyUp(upKey) || Input.GetKeyUp(downKey))
+        else
         {
-            directionY = 0;
-        }
-        if (Input.GetKeyUp(leftKey) || Input.GetKeyUp(rightKey))
-        {
-            directionX = 0;
+            if (xspeed > 0)
+            {
+                xspeed -= acceleration * Time.deltaTime;
+            }
+            if (xspeed < 0)
+            {
+                xspeed += acceleration * Time.deltaTime;
+            }
         }
     }
 }
