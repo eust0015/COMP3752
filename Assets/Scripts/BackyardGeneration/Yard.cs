33d9423947e98587fcf1713de55016e5e44442b6
Alex Morgan <43807058+Evi1Cat@users.noreply.github.com>
2022-08-17 23:36:28 +0930

    ...

---
+++ Assets/Scripts/BackyardGeneration/Yard.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Yard : MonoBehaviour
+{
+    public float xPosition;
+    public float yPosition;
+    public float pathDistance;
+    void Start(int mapSize)
+    {
+        if(mapSize > 0)
+        {
+
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
