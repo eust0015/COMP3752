96fe81c40b4c118491deaa7224aad49be4acd7c6
Kikaiji <60505347+Kikaiji@users.noreply.github.com>
2022-10-03 14:33:34 +1030

    Game manager base

--- Assets/Scripts/Systems/GameManager.cs
+++ Assets/Scripts/Systems/GameManager.cs
@@ -1,18 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager current { get; private set; }
+
+    private void Awake()
     {
-        
+        if (current != null && current != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            current = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public event Action onRoomStart;
+    protected virtual void RoomStart()
     {
-        
+        onRoomStart?.Invoke();
     }
+    
+    public event Action onRoomEnd;
+    protected virtual void RoomEnd()
+    {
+        onRoomEnd?.Invoke();
+    }
+    
+    public event Action onRunStart;
+    protected virtual void RunStart()
+    {
+        onRunStart?.Invoke();
+    }
+    
+    public event Action onRunEnd;
+    protected virtual void RunEnd()
+    {
+        onRunEnd?.Invoke();
+    }
+
+    public event Action onTimerPause;
+    protected virtual void OnOnTimerPause()
+    {
+        onTimerPause?.Invoke();
+    }
+
+    public event Action onTimerPlay;
+    protected virtual void OnOnTimerPlay()
+    {
+        onTimerPlay?.Invoke();
+    }
+    
+    public event Action onTimerComplete;
+
+    protected virtual void TimerComplete()
+    {
+        onTimerComplete?.Invoke();
+    }
+
+
+    
 }
