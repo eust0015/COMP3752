87fc839807dd3446b8cfe4e08a4584296ac6a9d4
Adam Eustis <eust0015@flinders.edu.au>
2022-10-23 11:43:29 +1030

    Added FMODAudioSource class to play sounds.

---
+++ Assets/Scripts/Audio/FMODAudioSource.cs
@@ -0,0 +1,15 @@
+﻿using FMODUnity;
+using UnityEngine;
+
+namespace Audio
+{
+    public class FMODAudioSource : MonoBehaviour
+    {
+        [SerializeField] public FMODUnity.EventReference sound;
+
+        public void PlaySound()
+        {
+            RuntimeManager.PlayOneShot(sound);
+        }
+    }
+}
