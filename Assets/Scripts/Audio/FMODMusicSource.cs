1643cd1c8f40371dce933fbc57770908714f0f1d
Adam Eustis <eust0015@flinders.edu.au>
2022-10-26 11:12:19 +1030

    Added a PlayPreviousMusic method

--- Assets/Scripts/Audio/FMODMusicSource.cs
+++ Assets/Scripts/Audio/FMODMusicSource.cs
@@ -9,6 +9,7 @@ namespace Audio
     public class FMODMusicSource : MonoBehaviour
     {
         public static FMODMusicSource currentlyPlayingMusic;
+        public static FMODMusicSource previouslyPlayingMusic;
         public FMOD.Studio.EventInstance instance;
         public FMODUnity.EventReference music; //music will be your music track & variables to control the music
         [SerializeField] private int playerHealth;
@@ -36,7 +37,10 @@ namespace Audio
 
         public void PlayMusic()
         {
-            currentlyPlayingMusic.instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            if (currentlyPlayingMusic != null)
+                currentlyPlayingMusic.instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            
+            previouslyPlayingMusic = currentlyPlayingMusic;
             currentlyPlayingMusic = this;
             instance = FMODUnity.RuntimeManager.CreateInstance(music); // This also works (like tutorial, older style)
             instance.start();
@@ -44,9 +48,24 @@ namespace Audio
             DetectNumberOfEnemies();
         }
 
-        public void StopMusic()
+        public static void PlayPreviousMusic()
         {
-            instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE); //Player is dead, stop music
+            StopMusic();
+            
+            if (previouslyPlayingMusic == null)
+                return;
+            
+            // Using deconstruction to swap currently playing music with previously playing music
+            (currentlyPlayingMusic, previouslyPlayingMusic) = (previouslyPlayingMusic, currentlyPlayingMusic);
+            currentlyPlayingMusic.instance.start();
+            currentlyPlayingMusic.DetectPlayerHealth();
+            currentlyPlayingMusic.DetectNumberOfEnemies();
+        }
+
+        public static void StopMusic()
+        {
+            if (currentlyPlayingMusic != null)
+                currentlyPlayingMusic.instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE); //Player is dead, stop music
         }
 
         public void DetectPlayerHealth()
